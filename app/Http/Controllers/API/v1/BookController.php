5aa646d82c070b197ec7e2f4478b1f6ecbc7b285
Dmitriy <dmitriy@MacBook-Pro-Dmitriy.local>
2020-05-17 21:50:26 +0500

    rest api refactoring

--- app/Http/Controllers/API/v1/BookController.php
+++ app/Http/Controllers/API/v1/BookController.php
@@ -8,45 +8,21 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return response() -> json(Book::with('author:id,name')->get());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return response() -> json(Book::find($id));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $book=Book::find($id);
@@ -58,12 +34,6 @@ class BookController extends Controller
         return response()->json($book, 201);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         Book::find($id)->delete();
